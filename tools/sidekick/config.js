8dc6f98f5cf05e198b622cadbea53762f655cc03
aclarke-adobe <69670375+aclarke-adobe@users.noreply.github.com>
2023-02-28 15:02:47 +1100

    Update config.js

--- tools/sidekick/config.js
+++ tools/sidekick/config.js
@@ -1,1 +1,14 @@
-
+window.hlx.initSidekick({
+    project: 'Eureka',
+    host: 'https://deft-hamster-eefe81.netlify.app',
+    plugins: [
+        { /* plugin config */
+            id: 'asset-library',
+            condition: true,
+            button: {
+                text: 'AEM Asset Library',
+                isDropdown: false,
+            }
+        },
+    ],
+});
